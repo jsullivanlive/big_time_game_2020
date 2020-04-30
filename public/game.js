4f6d2923b1b489263a0adf6c5f525bf1ada26337
James Sullivan <jsullivan@cloudanswers.com>
2020-05-01 00:21:15 +0200

    show ship on canvas

--- public/game.js
+++ public/game.js
@@ -19,10 +19,35 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image("ship", "/spaceshooter/PNG/playerShip1_blue.png");
+}
 
 function create() {
+  var self = this;
   this.socket = io();
+  this.socket.on("currentPlayers", function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id]);
+      }
+    });
+  });
 }
 
 function update() {}
+
+function addPlayer(self, playerInfo) {
+  self.ship = self.physics.add
+    .image(playerInfo.x, playerInfo.y, "ship")
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === "blue") {
+    self.ship.setTint(0x0000ff);
+  } else {
+    self.ship.setTint(0xff0000);
+  }
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+}
