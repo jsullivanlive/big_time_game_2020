9b35b5564ecf8e345dc2934babfe1cdaeccb7efa
James Sullivan <jsullivan@cloudanswers.com>
2020-05-01 09:09:48 +0200

    following tutorial at https://gamedevacademy.org/create-a-basic-multiplayer-game-in-phaser-3-with-socket-io-part-2/

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ io.on("connection", function (socket) {
     // emit a message to all players to remove this player
     io.emit("disconnect", socket.id);
   });
+
+  // when a player moves, update the player data
+  socket.on("playerMovement", function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit("playerMoved", players[socket.id]);
+  });
 });
 
 server.listen(process.env.PORT || 8081, function () {
